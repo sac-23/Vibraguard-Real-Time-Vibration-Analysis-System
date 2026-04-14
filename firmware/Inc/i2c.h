55ab47d3c9c16ddf7fd39784518faac11d263895
sac-23 <sachinnarendra23@gmail.com>
2026-04-14 23:07:30 +0530

    Add I2C function declarations in i2c.h

--- firmware/Inc/i2c.h
+++ firmware/Inc/i2c.h
@@ -1,1 +1,17 @@
 
+#ifndef I2C_H_
+#define I2C_H_
+
+void I2C1_Init(void);
+
+void I2C1_Start(void);
+void I2C1_Stop(void);
+
+void I2C1_WriteAddress(uint8_t address);
+void I2C1_WriteData(uint8_t data);
+uint8_t I2C1_ReadData_ACK(void);
+uint8_t I2C1_ReadData_NACK(void);
+
+
+#endif /* I2C_H_ */
+
